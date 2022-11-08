817abb9ca22e7a179f7e912b0509232d9b7e97ca
Je-Carlos <cpjeanberg@gmail.com>
2022-11-08 11:52:18 -0300

    Aula 2 completada

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,20 @@
+let name = 'Gon'
+var age = 20
+var time = null
+console.log(typeof time)
+
+var lastname = 'Freecss'
+let fullname = name + ' ' + lastname
+console.log(fullname)
+
+let ano = '2020'
+let mes = 4
+console.log(ano + mes)
+
+let teste = 'teste'
+let phrase = 'Eu sou um "teste" de frase' // Eu sou um "teste" de frase
+
+// template string
+let gols = 1000
+let frase = `Romario fez ${gols * 4} gols` // Romário fez 4000 gols
+console.log(frase)
