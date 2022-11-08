26cce24475b47cd55a165e99b6c4f580d477603d
Je-Carlos <cpjeanberg@gmail.com>
2022-11-08 11:07:23 -0300

    aula de variaveis feita

---
+++ variaveis/script.js
@@ -0,0 +1,15 @@
+var nome = 'Jean',
+  sobrenome = 'Cotillo',
+  idade = 21,
+  possuiFaculdade = true
+
+console.log(nome, sobrenome, idade, possuiFaculdade)
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = preco * totalComprado
+
+console.log(totalPreco)
+
+var semDefinir; // undefined
+console.log(semDefinir)
