04f7cc631cbdcb3f042f95fea822bc244a9b0632
Je-Carlos <cpjeanberg@gmail.com>
2022-11-09 14:57:00 -0300

    aula tudo é objeto + exercicios feitos

---
+++ tudo-e-objeto/script.js
@@ -0,0 +1,13 @@
+var nome = "Tudo é objeto";
+var nomeMaisculo = nome.toUpperCase();
+
+var altura = 13.4;
+
+altura.toString();
+altura.toFixed();
+// Por um breve momento o número é envolvido em um Objeto (coerção), 
+// tendo acesso assim as suas propriedades e métodos
+
+// Elementos do DOM -
+ var btn = document.querySelector(".btn");
+ 
