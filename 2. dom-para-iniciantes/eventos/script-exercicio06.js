42b743da20587f809f09700af73924ef88c2fa80
Je-Carlos <cpjeanberg@gmail.com>
2022-11-22 13:21:46 -0300

    aula eventos + exercicios feitos

--- 2. dom-para-iniciantes/eventos/script-exercicio06.js
+++ 2. dom-para-iniciantes/eventos/script-exercicio06.js
@@ -1,41 +1,44 @@
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
-const distanciaImg = document.querySelector("img");
-const altura = distanciaImg.offsetTop;
-console.log(altura);
-// Retorne a soma da largura de todas as imagens
-function somaImagens() {
-  const imagens = document.querySelectorAll("img");
-  let soma = 0;
-  imagens.forEach((imagem) => {
-    soma += imagem.offsetWidth;
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+
+const links = document.querySelectorAll("a[href^='#']");
+function handleLinks(event) {
+  event.preventDefault();
+  links.forEach((rmvLink) => {
+    rmvLink.classList.remove("ativo");
   });
-  console.log(soma);
+  this.classList.add("ativo");
 }
-window.onload = function () {
-  somaImagens();
-};
 
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-const linksMinimo = document.querySelectorAll("a");
-
-linksMinimo.forEach((links) => {
-  const linkWidth = links.offsetWidth;
-  const linkHeight = links.offsetHeight;
-
-  if (linkHeight >= 48 && linkWidth >= 48) {
-    console.log(links, "Possui boa acessibilidade");
-  } else {
-    console.log(links, "Não possuem boa acessibilidade");
-  }
+links.forEach((link) => {
+  link.addEventListener("click", handleLinks);
 });
 
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
-const menu = document.querySelector(".menu");
-const browserValor = window.matchMedia("(max-width: 720px)").matches;
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+const elementosBody = document.querySelectorAll("body *");
+function handleElementos(event) {
+  console.log(event.currentTarget);
+}
+elementosBody.forEach((elementos) => {
+  elementos.addEventListener("click", handleElementos);
+});
 
-if (browserValor) menu.classList.add("menu-mobile");
-else console.log("O valor é maior que 720px");
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+const rmvElementos = document.querySelectorAll("body *");
+function handleRemove(event) {
+  event.currentTarget.remove();
+}
+rmvElementos.forEach((e) => {
+  e.addEventListener("click", handleRemove);
+});
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
+function screenSize(event) {
+  if(event.key === "t") {
+    document.documentElement.classList.toggle("aumentarTexto")
+  }
+}
+window.addEventListener("keydown", screenSize);
--- 2. dom-para-iniciantes/eventos/script.js
+++ 2. dom-para-iniciantes/eventos/script.js
@@ -31,10 +31,68 @@ function executarCallback(event) {
   console.log(event.currentTarget); // this
   console.log(event.target);
   console.log(event.type);
-
 }
 animaisLista.addEventListener("click", executarCallback);
 
 // event.preventDefault(); -
-// Previne o comportamento padrão do evento no browser. 
+// Método que previne o comportamento padrão do evento no browser.
 // No caso de um link externo, por exemplo, irá previnir que o link seja ativado.
+
+const linkExterno = document.querySelector("a[href^='http']");
+function handleLinkExterno(event) {
+  event.preventDefault();
+  console.log(event);
+}
+linkExterno.addEventListener("click", handleLinkExterno);
+
+// this -
+// A palavra chave this é uma palavra especial de JavaScript,
+// que pode fazer referência a diferentes objetos dependendo do contexto.
+// No caso de eventos, ela fará referência ao elemento em que addEventListener foi adicionado.
+
+const thisImg = document.querySelector("img");
+function handleLinkThis(event) {
+  event.preventDefault();
+  console.log(this);
+  console.log(event.currentTarget); // Retorna o mesmo valor que this
+  console.log(this.getAttribute("src"));
+}
+thisImg.addEventListener("click", handleLinkThis);
+
+// Diferente Eventos -
+// Existem diversos eventos como click, scroll, resize, keydown, keyup,
+// mouseenter e mais. Eventos podem ser adicionados a diferentes elementos,
+// como o window e document também.
+
+const h1 = document.querySelector("h1");
+function handleEvent(event) {
+  console.log(event.type, event);
+}
+// h1.addEventListener("click", handleEvent);
+// h1.addEventListener("mouseenter", handleEvent);
+
+// // window - eventos gerais
+// window.addEventListener("scroll", handleEvent);
+// window.addEventListener("resize", handleEvent);
+// window.addEventListener("keydown", handleEvent);
+
+function handleKeyboard(event) {
+  // muda a cor da pagina conforme aperta J ou H
+  if (event.key === "j")
+    document.body.classList.toggle("roxo"); // muda a classe do body
+  else if (event.key === "h") document.body.classList.toggle("rosa");
+}
+window.addEventListener("keydown", handleKeyboard);
+
+// forEach() e eventos
+// O método addEventListener é adicionado a um único elemento, então é necessário
+// fazer um loop entre os elementos da lista.
+
+const srcImgs = document.querySelectorAll("img");
+function handleImgSrc(event) {
+  const src = event.currentTarget.getAttribute("src");
+  console.log(src);
+}
+srcImgs.forEach((img) => {
+  img.addEventListener("click", handleImgSrc);
+});
