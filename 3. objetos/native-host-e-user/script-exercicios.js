136488532d0c9b4c094a2d123f4bdf16668b54d5
Je-Carlos <cpjeanberg@gmail.com>
2022-12-20 14:36:34 -0300

    native, host e user + exerc

---
+++ 3. objetos/native-host-e-user/script-exercicios.js
@@ -0,0 +1,27 @@
+// Liste 5 objetos nativos
+
+/*
+Object
+String
+Array
+Function
+Boolean
+*/
+
+// Liste 5 objetos do browser
+
+/*
+NodeList
+HTMLCollection
+Window
+Document
+Element
+*/
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+/*
+document.webkitHidden
+document.webkitVisibilityState
+*/
---
+++ 3. objetos/native-host-e-user/script.js
@@ -0,0 +1,5 @@
+// Verificar se existe
+console.log(typeof Array.fromEuSouUmErro !== "undefined")
+
+if (typeof Array.fromEuSouUmTeste !== "undefined") console.log("Existe")
+else console.log("Não Existe")
